fe46889d27f09ca901b720eb886dee5058bb1392
Ozami Duque Rodriguez <ozamiduro.dev@gmail.com>
2024-10-06 01:33:47 -0500

    Allow cors

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI
 from app.routes import evaluate_data, metadata
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize the FastAPI application with metadata for documentation
 app = FastAPI(
@@ -9,6 +10,17 @@ app = FastAPI(
     description="Web application to compare ground-based observations with Landsat satellite data.",
     version="1.0.0"
 )
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include routers for different endpoints with appropriate tags for documentation
 app.include_router(evaluate_data.router, tags=["Evaluate Data"])
